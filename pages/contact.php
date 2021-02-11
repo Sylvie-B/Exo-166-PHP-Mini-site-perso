2f6aa841eaae4392ceb481b0f5fd9c0f1806c4c3
Sylvie-B <unsolo@hotmail.fr>
2021-02-11 16:01:19 +0100

    contact page setting

--- pages/contact.php
+++ pages/contact.php
@@ -1,16 +1,23 @@
 <section>
-    <h1>Contact</h1>
+    <h1>CONTACT</h1>
+    <div id="contact">
+        <form method="post" action="../public/save.php">
+            <div>
+                <label for="userName">votre nom</label>
+                <input type="text" id="userName">
+            </div>
+            <div>
+                <label for="message">votre message</label>
+                <textarea name="userTxt" id="message" cols="50" rows="10"></textarea>
+            </div>
+            <button type="submit">valider</button>
+        </form>
 
-    <form method="post" action="../public/save.php">
-        <div>
-            <label for="userName">votre nom</label>
-            <input type="text" id="userName">
+        <div id="socials">
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-twitch"></i>
         </div>
-        <div>
-            <label for="message">votre message</label>
-            <textarea name="userTxt" id="message" cols="30" rows="10"></textarea>
-        </div>
-        <button type="submit">valider</button>
-    </form>
-
+    </div>
 </section>
--- parts/header.php
+++ parts/header.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, user-scalable=no">
         <title>Sylvie Bataille</title>
         <link rel="stylesheet" href="../public/style.css">
+        <script src="https://kit.fontawesome.com/b3c591f1b4.js" crossorigin="anonymous"></script>
     </head>
     <body>
         <header>
